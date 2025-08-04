a22dff92cc056f2e91958222e3e59f7bac686ac8
FazleyRabbi04 <akmfazley@gmail.com>
2025-08-04 22:48:13 +0600

    signup update

--- auth/signup.php
+++ auth/signup.php
@@ -260,9 +260,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <label for="gender" class="form-label">Gender</label>
             <select name="gender" class="form-control" id="gender" required>
                 <option value="">Select Gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
             </select>
         </div>
         <div class="mb-3">
